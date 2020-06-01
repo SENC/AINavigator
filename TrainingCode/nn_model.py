20ca673a3b66d4365ba3da0f732a949bacf90482
SENTHIL CHIDAMBARAM <SENTHILC78@HOTMAIL.COM>
2020-06-01 18:16:03 +0530

    Update nn_model.py
    torch.manual_seed function corrected

--- TrainingCode/nn_model.py
+++ TrainingCode/nn_model.py
@@ -14,7 +14,7 @@ class QDeepNetwork(nn.Module):
     def __init__(self, state_size,action_size,seed, fc1_neurons=64,fc2_neurons=64):
         
         super(QDeepNetwork,self).__init__()
-        self.seed = torch.manual.seed(seed)
+        self.seed = torch.manual_seed(seed)
         self.fc1  = nn.Linear(state_size,fc1_neurons)
         self.fc2  = nn.Linear(fc1_neurons, fc2_neurons)
         self.fc3  = nn.Linear(fc2_neurons,action_size)
