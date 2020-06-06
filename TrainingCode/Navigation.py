23378a71f9b1e3c125306deef03c7ff8a0cdf220
SENTHIL CHIDAMBARAM <SENTHILC78@HOTMAIL.COM>
2020-06-06 16:00:27 +0530

    Update Navigation.py
    Updated with Test run with the trained Agent

--- TrainingCode/Navigation.py
+++ TrainingCode/Navigation.py
@@ -11,7 +11,20 @@
 # 
 # Run the next code cell to install a few packages.  This line will take a few minutes to run!
 
-# In[1]:
+# In[2]:
+
+
+#Reference Libraries
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import random
+import torch
+
+
+# In[3]:
 
 
 get_ipython().system('pip -q install ./python')
@@ -19,7 +32,7 @@ get_ipython().system('pip -q install ./python')
 
 # The environment is already saved in the Workspace and can be accessed at the file path provided below.  Please run the next code cell without making any changes.
 
-# In[2]:
+# In[4]:
 
 
 from unityagents import UnityEnvironment
@@ -31,7 +44,7 @@ env = UnityEnvironment(file_name="/data/Banana_Linux_NoVis/Banana.x86_64")
 
 # Environments contain **_brains_** which are responsible for deciding the actions of their associated agents. Here we check for the first brain available, and set it as the default brain we will be controlling from Python.
 
-# In[3]:
+# In[5]:
 
 
 # get the default brain
@@ -44,7 +57,7 @@ print(brain)
 
 # 
 
-# In[4]:
+# In[6]:
 
 
 # reset the environment
@@ -70,7 +83,7 @@ print('States have length:', state_size)
 # 
 # Note that **in this coding environment, you will not be able to watch the agent while it is training**, and you should set `train_mode=True` to restart the environment.
 
-# In[5]:
+# In[6]:
 
 
 env_info = env.reset(train_mode=True)[brain_name] # reset the environment
@@ -91,7 +104,11 @@ while True:
 print("Score: {}".format(score))
 
 
-# In[6]:
+# ### 4. Instantiate AI Agent and Train  in the Environment 
+# 
+# 
+
+# In[7]:
 
 
 #DeepQLearning 
@@ -99,35 +116,52 @@ from nn_model import QDeepNetwork
 from agent import AiAgent
 
 
-
-
-
-
-# In[7]:
-
-
-#Reference Libraries
-import numpy as np
-from collections import deque
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-import random
-import torch
-
-
-# In[ ]:
+# In[8]:
 
 
 #Test to create the instance of AiAgent
 firstAI = AiAgent(state_size=37,action_size=4,seed=17)
 
 
-# In[44]:
+# In[9]:
 
 
-#Train the Agent
-#Function dqn helps to calculate Agent's score when it get interacted with the Environment
+# Make the Agent interacted with its Environment in single Episode -1000 steps
+def test_run_single_episode (env: UnityEnvironment,brain_name, ai_agent:AiAgent=None,max_t=1000,epsl=0.,train_mode=False):
+    """Input env|agent|number of steps|test mode
+       Output Agent score
+    
+    """
+    env_info = env.reset(train_mode=train_mode)[brain_name]
+    action_size = env.brains[brain_name].vector_action_space_size
+    state = env_info.vector_observations[0]
+    
+    #initial the score 
+    score =0
+    
+    #Run each step in the episode
+    for _ in range (max_t):
+        action = ai_agent.act(state,epsl) if ai_agent else np.random.randint(action_size)
+        
+        #lets make the agent take the action 
+        env_info = env.step(action)[brain_name]
+        
+        #get the next state 'St+1'
+        next_state = env_info.vector_observations[0]
+        reward = env_info.rewards[0]
+        done = env_info.local_done[0]
+        
+        if ai_agent and train_mode: #Train mode is True then make the agent learn
+            ai_agent.step(state, action, reward, next_state, done)
+        
+        state = next_state
+        score += reward
+        if done:
+            break
+    return score
+
+#Train the Agent - DeepQ Network Algorithm
+#Function dqn helps to calculate Agent's optimized score to choose Greedy Policy based on rewards
 def dqn(ai_agent,num_episodes=1000, max_t=1000, eps_start=0.78, eps_end=0.0025, eps_decay=0.995):
     """Deep Q-Learning.
     
@@ -164,7 +198,7 @@ def dqn(ai_agent,num_episodes=1000, max_t=1000, eps_start=0.78, eps_end=0.0025, 
             reward = env_info.rewards[0]                   # get the reward
             done = env_info.local_done[0]                  # see if episode has finished 
             
-            ai_agent.step(state, action, reward, next_state, done)
+            ai_agent.step(state, action, reward, next_state, done) # Replay Memory - Learn from potential failure
             
             score += reward                                # update the score
             state = next_state                             # roll over the state to next time step            
@@ -177,14 +211,91 @@ def dqn(ai_agent,num_episodes=1000, max_t=1000, eps_start=0.78, eps_end=0.0025, 
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-        if np.mean(scores_window)>=15.0:
+        if np.mean(scores_window)>=17.0:
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
-            torch.save(ai_agent.qnetwork_local.state_dict(), 'checkpointai15.pth')
+            torch.save(ai_agent.qnetwork_local.state_dict(), 'checkpointai17.pth')
             break
         
     return scores      
        
 
+
+# In[11]:
+
+
+#v9.4 -Highy score so repeat once  to get threshold 15
+firstAI1 = AiAgent(state_size=37,action_size=4,seed=24)
+
+num_episodes=10000
+max_t=1000
+eps_start=0.9786 
+eps_end=0.0005 
+eps_decay=0.9799
+scores = dqn(firstAI1,num_episodes,max_t,eps_start,eps_end,eps_decay)
+
+
+# In[12]:
+
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+
+
+# In[10]:
+
+
+#Watch the train Agent perfromance
+# load the weights from file
+firstAI_trained = AiAgent(state_size=37,action_size=4,seed=24)
+firstAI_trained.qnetwork_local.load_state_dict(torch.load('checkpointai17.pth'))
+          
+
+
+# In[11]:
+
+
+# Test Run score
+
+score = test_run_single_episode(env, brain_name, firstAI_trained)
+
+print(f'Score: {score}')
+
+
+# ## Check the Trained Agent Performance 
+# 
+# Based on the training weight, run the agent in the same environment in Test Mode (train_mode= False) and Goal to achieve 13+ as an average score
+
+# In[14]:
+
+
+scores_window = deque(maxlen=50)
+for test_episode in range(1,51):
+    score = test_run_single_episode(env, brain_name, firstAI_trained)
+    print(f'Score: {score}')    
+    scores_window.append(score)       # save most recent score
+    print('\rEpisode {}\tAverage Score: {:.2f}\t'.format(test_episode, np.mean(scores_window)), end= "")
+
+
+# In[13]:
+
+
+for _ in range (10):
+    score = test_run_single_episode(env, brain_name, firstAI_trained)
+    print(f'Score: {score}')
+
+
+# In[17]:
+
+
+env.close()
+
+
+# #History - Parameter tuning 
 
 # In[45]:
 
@@ -199,45 +310,6 @@ eps_end=0.0005
 eps_decay=0.979
 scores = dqn(firstAI1,num_episodes,max_t,eps_start,eps_end,eps_decay)
 
-
-# In[46]:
-
-
-# plot the scores
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(np.arange(len(scores)), scores)
-plt.ylabel('Score')
-plt.xlabel('Episode #')
-plt.show()
-
-
-# In[47]:
-
-
-#Watch the train Agent perfromance
-# load the weights from file
-firstAI_trained = AiAgent(state_size=37,action_size=4,seed=24)
-firstAI_trained.qnetwork_local.load_state_dict(torch.load('checkpointai15.pth'))
-          
-
-
-# In[48]:
-
-
-# Test Run score
-score = test_run_single_episode(env, brain_name, firstAI_trained)
-
-print(f'Score: {score}')
-
-
-# In[49]:
-
-
-env.close()
-
-
-# #History - Parameter tuning 
 
 # In[42]:
 
